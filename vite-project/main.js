0cd2027f246486103b1175b8fef74cbcf5480746
Giovanni <91745260+Oscaro2897@users.noreply.github.com>
2023-04-25 23:48:37 +0000

    Tue, Apr 25, 2023, 7:48 PM -04:00

--- vite-project/main.js
+++ vite-project/main.js
@@ -22,7 +22,7 @@ document.querySelector('#app').innerHTML = `
 `
 
   setupCounter(document.querySelector('#counter'))
-  
+
   import * as THREE from 'three';
 
   const scene = new THREE.Scene();
@@ -31,3 +31,18 @@ document.querySelector('#app').innerHTML = `
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
+
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
+cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
