22d3ea9dc1f7b0e1b1285a362ecf7531d080235e
Giovanni <91745260+Oscaro2897@users.noreply.github.com>
2023-05-16 23:39:07 +0000

    Tue, May 16, 2023, 7:39 PM -04:00

--- vite-project/main.js
+++ vite-project/main.js
@@ -19,33 +19,40 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`import * as THREE from 'three';
 
-  setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  // setupCounter(document.querySelector('#counter'))
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  // const scene = new THREE.Scene();
+  // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  camera.position.z = 5;
+  // const renderer = new THREE.WebGLRenderer();
+  // renderer.setSize( window.innerWidth, window.innerHeight );
+  // document.body.appendChild( renderer.domElement );
 
-  function animate() {
-    requestAnimationFrame( animate );
+  // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  // const cube = new THREE.Mesh( geometry, material );
+  // scene.add( cube );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+  // camera.position.z = 5;
 
-    renderer.render( scene, camera );
-  }
+  // function animate() {
+  //   requestAnimationFrame( animate );
 
-  animate();
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+
+  //   renderer.render( scene, camera );
+  // }
+
+  // animate();
 
   //remeber to use 'npm run dev'
