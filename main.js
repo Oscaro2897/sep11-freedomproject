7abff929cb84c0dd2fdc58f5b18b46022d05ad1e
Giovanni <91745260+Oscaro2897@users.noreply.github.com>
2023-05-24 00:57:20 +0000

    Tue, May 23, 2023, 8:57 PM -04:00

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ if ( WebGL.isWebGLAvailable() ) {
 
 }
 
-// drawing lines
+// drawing
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
